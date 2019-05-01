5794d24171010e329f2275932674783ebc261160
Mike Egner <egg-meister@hotmail.com>
2019-05-01 21:54:10 +0200

    added placeholder Instagram feed to gallery

--- src/components/gallery/GalleryPage.js
+++ src/components/gallery/GalleryPage.js
@@ -1,10 +1,32 @@
 import React from "react";
+import InstagramEmbed from "react-instagram-embed";
 
 const GalleryPage = () => {
   return (
     <>
       <h1>Gallery Page</h1>
-      <div>This is where photos from Instagram must go</div>
+      <div className="my-3">
+        Our resident photographer and constant gardener, Judy Green, takes most
+        of the photographs that have made Misty Valley famous.
+      </div>
+      <div className="container">
+        <div className="row justify-content-md-center">
+          <div className="col-10">
+            <InstagramEmbed
+              url="https://www.instagram.com/p/BvegtOJA97H/"
+              maxWidth={640}
+              hideCaption={false}
+              containerTagName="div"
+              protocol=""
+              injectScript
+              onLoading={() => {}}
+              onSuccess={() => {}}
+              onAfterRender={() => {}}
+              onFailure={() => {}}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
